067ed608daa687c3e403563fde5b663a159258fe
nancytaswala23 <taswala.n@northeastern.edu>
2024-12-02 13:48:57 -0500

    pushing
    pushing java files

---
+++ Final_Project/src/HealthCare/Encounter.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HealthCare;
+
+/**
+ *
+ * @author taswa
+ */
+public class Encounter {
+    public Doctor doctor;
+    public Patient patient;
+    public String vitalSigns;
+
+    public Encounter(Doctor doctor, Patient patient) {
+        this.doctor = doctor;
+        this.patient = patient;
+        vitalSigns = "";
+    }
+    
+}
